e112f09d48842e4283537a4f8f57348b7ccaeec2
Rohan Kumar <rohank657@gmail.com>
2020-07-16 20:06:37 +0530

    first

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+{id: 11, name: 'Dr Nice'},
+{id: 12, name: 'Rohan'},
+{id: 13, name: 'Magenta'},
+{id: 14, name: 'Iron Man'},
+{id: 15, name: 'Superman'},
+{id: 16, name: 'Captain America'},
+{id: 17, name: 'Hulk'},
+{id: 18, name: 'Sunil'},
+{id: 19, name: 'Ritu'},
+{id: 20, name: 'Batman'},
+];
